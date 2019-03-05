1e152586228bbcff596c488e43d5d455e14d7411
goodspellar <goodspellar@users.noreply.github.com>
2019-03-04 20:51:52 -0800

    ..

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,25 @@
+import random
+
 class Robot(object):
+
+    name_list = []
+
     def __init__(self):
-        pass
+        self.name = self.get_new_name()
+
+    def get_new_name(self):
+        while True:
+            name = self.create_name()
+            if name not in self.name_list:
+                return name
+
+    def create_name(self):
+        return chr(random.randint(65, 90)) + \
+            chr(random.randint(65,90)) + \
+                str(random.randint(0,9)) + \
+                    str(random.randint(0,9)) + \
+                        str(random.randint(0,9)) 
+
+    def reset(self):
+        self.name_list.append(self.name)
+        self.name = self.get_new_name()
